5699b584b47af9fb39d2b975345920c8842583a0
Ingerid Dale <ingerid.dale@nb.no>
2025-01-23 17:58:05 +0100

    feat: add wrapper function for semdist

--- src/asr_eval/metrics.py
+++ src/asr_eval/metrics.py
@@ -1,17 +1,22 @@
+"""Wrapper module for Automatic Speech Recognition evaluation metrics"""
 import jiwer
+from asr_eval.semantic_distance import calculate_semdist
+from transformers import BertTokenizer, BertForMaskedLM
 
 
 def cer(reference: str, hypothesis: str) -> float:
+    """Calculate Character Error Rate (CER) between reference and hypothesis"""
     return jiwer.cer(reference=reference, hypothesis=hypothesis)
 
 
 def wer(reference: str, hypothesis: str) -> float:
+    """Calculate Word Error Rate (WER) between reference and hypothesis"""
     return jiwer.wer(reference=reference, hypothesis=hypothesis)
 
 
-
-def semdist(row): 
-    pass 
+def semdist(reference: str, hypothesis: str, model: BertForMaskedLM, tokenizer: BertTokenizer) -> float: 
+    """Calculate semantic distance between reference and hypothesis"""
+    return calculate_semdist(reference=reference, hypothesis=hypothesis, model=model, tokenizer=tokenizer)
 
 
 def sbert_semdist(row):
