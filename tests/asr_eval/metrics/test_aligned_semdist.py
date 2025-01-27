4e62b2a6c9e7a631b2f790d8975483f5ae4c6ca9
Ingerid Dale <ingerid.dale@nb.no>
2025-01-27 15:32:17 +0100

    aligned semdist returns the float score

--- tests/asr_eval/metrics/test_aligned_semdist.py
+++ tests/asr_eval/metrics/test_aligned_semdist.py
@@ -13,4 +13,4 @@ from asr_eval.metrics import aligned_semdist
 )
 def test_returns_0_for_equal_strings(reference, hypothesis, bert_tokenizer, bert_model):
     result = aligned_semdist(reference=reference, hypothesis=hypothesis, model=bert_model, tokenizer=bert_tokenizer)
-    assert result.score == 0
+    assert result == 0
