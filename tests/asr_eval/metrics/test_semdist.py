b7cbb33ed06f88a5dd12cbe4ef00bfa77d618a1c
Ingerid Dale <ingerid.dale@nb.no>
2025-01-27 15:33:05 +0100

    tests: allow semdist to return approximately 0 floats

--- tests/asr_eval/metrics/test_semdist.py
+++ tests/asr_eval/metrics/test_semdist.py
@@ -13,4 +13,4 @@ from asr_eval.metrics import semdist
 )
 def test_returns_0_for_equal_strings(reference, hypothesis, bert_tokenizer, bert_model):
     result = semdist(reference=reference, hypothesis=hypothesis, model=bert_model, tokenizer=bert_tokenizer)
-    assert result == 0
+    assert result == pytest.approx(0, rel=1e-5)
