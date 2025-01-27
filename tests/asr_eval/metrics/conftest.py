18748b4324c3cb6dca2d6a68e083b676255d6d8d
Ingerid Dale <ingerid.dale@nb.no>
2025-01-27 09:55:18 +0100

    tests: add test objects (pytest fixtures)

---
+++ tests/asr_eval/metrics/conftest.py
@@ -0,0 +1,26 @@
+import pytest
+from sentence_transformers import SentenceTransformer
+
+from transformers import AutoTokenizer, AutoModelForMaskedLM
+
+
+@pytest.fixture(scope="session")
+def sbert_model():
+    model = SentenceTransformer('NbAiLab/nb-sbert-base') # TODO: bytt til riktig modell 
+    return model
+
+
+@pytest.fixture(scope="session")
+def bert_model():
+    """Load a "small" Norwegian BERT model to run with the tests, which can also be run on CPU"""
+    bertmodelname = "NbAiLab/nb-bert-base"
+    model = AutoModelForMaskedLM.from_pretrained(bertmodelname, trust_remote_code=True)
+    return model
+
+
+@pytest.fixture(scope="session")
+def bert_tokenizer():
+    """Load a BERT tokenizer"""
+    bertmodelname = "NbAiLab/nb-bert-base"
+    tokenizer = AutoTokenizer.from_pretrained(bertmodelname)
+    return tokenizer
