410c507d8f80c6171a67152728e67fc1193febc9
Ingerid Dale <ingerid.dale@nb.no>
2025-01-29 17:14:46 +0100

    tests: compare floats to be approximately the same, within the 6 decimals

--- tests/asr_eval/metrics/test_sbert_semdist.py
+++ tests/asr_eval/metrics/test_sbert_semdist.py
@@ -8,9 +8,9 @@ from asr_eval.metrics import sbert_semdist
         ("foo foo foo", "foo foo foo"),
         ("Setninger kan være korte.", "Setninger kan være korte."),
         ("Noen er lange.", "Noen er lange."),
-        ("Ord", "ord"),
+        ("Ord", "Ord"),
     ],
 )
 def test_returns_0_for_equal_strings(reference, hypothesis, sbert_model):
     result = sbert_semdist(reference=reference, hypothesis=hypothesis, model=sbert_model)
-    assert result == pytest.approx(0, rel=1e-3)
+    assert result == pytest.approx(0.0, abs=1e-6)
--- tests/asr_eval/metrics/test_semdist.py
+++ tests/asr_eval/metrics/test_semdist.py
@@ -8,9 +8,9 @@ from asr_eval.metrics import semdist
         ("foo foo foo", "foo foo foo"),
         ("Setninger kan være korte.", "Setninger kan være korte."),
         ("Noen er lange.", "Noen er lange."),
-        ("Ord", "ord"),
+        ("Ord", "Ord"),
     ],
 )
 def test_returns_0_for_equal_strings(reference, hypothesis, bert_tokenizer, bert_model):
     result = semdist(reference=reference, hypothesis=hypothesis, model=bert_model, tokenizer=bert_tokenizer)
-    assert result == pytest.approx(0, rel=1e-5)
+    assert result == pytest.approx(0.0, abs=1e-6)
